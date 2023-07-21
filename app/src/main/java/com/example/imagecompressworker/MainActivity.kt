b0dc8e7d6fd1e82f7c9ae5bd584dc0e0a7735367
wakelift <esazlesa@gmail.com>
2023-07-21 04:24:33 +0300

    done

--- app/src/main/java/com/example/imagecompressworker/MainActivity.kt
+++ app/src/main/java/com/example/imagecompressworker/MainActivity.kt
@@ -1,31 +1,111 @@
 package com.example.imagecompressworker
 
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import coil.compose.AsyncImage
 import com.example.imagecompressworker.ui.theme.ImageCompressWorkerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private lateinit var workManager: WorkManager
+    private val viewModel by viewModels<PhotoViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        workManager = WorkManager.getInstance(applicationContext)
+
         setContent {
             ImageCompressWorkerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val workerResult = viewModel.workId?.let { id ->
+                    workManager.getWorkInfoByIdLiveData(id).observeAsState().value
                 }
+                Log.i(TAG, "workerResult: id=${workerResult?.id}, keyValueMap=${workerResult?.outputData?.keyValueMap}")
+                LaunchedEffect(key1 = workerResult?.outputData) {
+                    if (workerResult?.outputData != null) {
+                        val filePath = workerResult.outputData
+                            .getString(PhotoCompressionWorker.KEY_RESULT_PATH)
+                        Log.i(TAG, "LE: filepath=$filePath")
+                        filePath?.let {
+                            val bitmap = BitmapFactory.decodeFile(it)
+                            viewModel.updateCompressedBitmap(bitmap)
+                        }
+                    }
+                }
+
+                Toast.makeText(this, "LAUNCH", Toast.LENGTH_SHORT).show()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text("HEADER")
+                    Spacer(modifier = Modifier.height(16.dp))
+                    viewModel.uncompressedUri?.let { uri ->
+                        Text(text = "Uncompressed photo:")
+                        AsyncImage(model = uri, contentDescription = "uncompressed")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    viewModel.compressedBitmap?.let { bitmap ->
+                        Text(text = "Compressed photo")
+                        Image(bitmap = bitmap.asImageBitmap(), contentDescription = "compressed")
+                    }
+                }
+
             }
         }
+
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Toast.makeText(this, "start onNewIntent // IF TIRAMISU", Toast.LENGTH_SHORT).show()
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            Toast.makeText(this, "start onNewIntent // IF ELSE", Toast.LENGTH_SHORT).show()
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM)
+        } ?: return
+        viewModel.updateUncompressUri(uri)
+
+        val request = OneTimeWorkRequestBuilder<PhotoCompressionWorker>()
+            .setInputData(
+                workDataOf(
+                    PhotoCompressionWorker.KEY_CONTENT_URI to uri.toString(),
+                    PhotoCompressionWorker.KEY_COMPRESSION_THRESHOLD to 1024 * 20L
+                )
+            )
+//            .setConstraints(Constraints(requiresStorageNotLow = true))
+            .build()
+        viewModel.updateWorkId(request.id)
+
+        workManager.enqueue(request)
     }
 }
 
